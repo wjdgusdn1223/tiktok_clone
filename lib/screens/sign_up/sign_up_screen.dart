dc41bbe65728421cbfad2350e27cce74d8bfa281
Hyeonwoo Jung <wjdgusdn1223@gmail.com>
2023-08-25 00:38:20 +0900

    sign up screen first commit

---
+++ lib/screens/sign_up/sign_up_screen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+
+class SignUpScreen extends StatelessWidget {
+  const SignUpScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: Sizes.size40),
+          child: Column(
+            children: [
+              Gaps.v80,
+              Text(
+                'Sign up for TikTok',
+                style: TextStyle(
+                  fontSize: Sizes.size24,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              Gaps.v20,
+              Text(
+                'Create a profile, follow other accounts, make your own videos, and more.',
+                style: TextStyle(
+                  fontSize: Sizes.size14,
+                  color: Colors.black45,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        padding: const EdgeInsets.symmetric(
+          vertical: Sizes.size20,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Already have an account?',
+            ),
+            Gaps.h5,
+            Text(
+              'Log in',
+              style: TextStyle(
+                color: Theme.of(context).primaryColor,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
