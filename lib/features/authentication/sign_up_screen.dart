7bef4148469575e129960beda0e758887d8fe121
Hyeonwoo Jung <wjdgusdn1223@gmail.com>
2023-08-29 00:42:18 +0900

    authentication feature fourth commit

--- lib/features/authentication/sign_up_screen.dart
+++ lib/features/authentication/sign_up_screen.dart
@@ -4,6 +4,7 @@ import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:tiktok_clone/features/authentication/email_screen.dart';
 import 'package:tiktok_clone/features/authentication/login_screen.dart';
+import 'package:tiktok_clone/features/authentication/username_screen.dart';
 import 'package:tiktok_clone/features/authentication/widgets/auth_button.dart';
 
 class SignUpScreen extends StatelessWidget {
@@ -20,7 +21,7 @@ class SignUpScreen extends StatelessWidget {
   void _onEmailAndPasswordTap(BuildContext context) {
     Navigator.of(context).push(
       MaterialPageRoute(
-        builder: (context) => const EmailScreen(),
+        builder: (context) => const UsernameScreen(),
       ),
     );
   }
--- lib/features/authentication/username_screen.dart
+++ lib/features/authentication/username_screen.dart
@@ -1,10 +1,107 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
 
-class UsernameScreen extends StatelessWidget {
+class UsernameScreen extends StatefulWidget {
   const UsernameScreen({super.key});
 
   @override
+  State<UsernameScreen> createState() => _UsernameScreenState();
+}
+
+class _UsernameScreenState extends State<UsernameScreen> {
+  final TextEditingController _usernameController = TextEditingController();
+
+  String _username = "";
+
+  @override
+  void initState() {
+    super.initState();
+
+    _usernameController.addListener(() {
+      setState(() {
+        _username = _usernameController.text;
+      });
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sign up'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: Sizes.size36,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Gaps.v40,
+            const Text(
+              'Create username',
+              style: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: Sizes.size20,
+              ),
+              textAlign: TextAlign.left,
+            ),
+            Gaps.v10,
+            const Text(
+              'You can always change this later',
+              style: TextStyle(
+                fontSize: Sizes.size16,
+                color: Colors.black54,
+              ),
+            ),
+            Gaps.v16,
+            TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(
+                hintText: "Username",
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.grey.shade400,
+                  ),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.grey.shade400,
+                  ),
+                ),
+              ),
+              cursorColor: Theme.of(context).primaryColor,
+            ),
+            Gaps.v16,
+            FractionallySizedBox(
+              widthFactor: 1,
+              child: AnimatedContainer(
+                duration: const Duration(milliseconds: 200),
+                padding: const EdgeInsets.symmetric(vertical: Sizes.size12),
+                decoration: BoxDecoration(
+                  color: _username.isEmpty
+                      ? Colors.grey.shade100
+                      : Theme.of(context).primaryColor,
+                  borderRadius: BorderRadius.circular(
+                    Sizes.size5,
+                  ),
+                ),
+                child: Text(
+                  'Next',
+                  style: TextStyle(
+                      color: _username.isEmpty
+                          ? Colors.grey.shade400
+                          : Colors.white,
+                      fontSize: Sizes.size16,
+                      fontWeight: FontWeight.w600),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
